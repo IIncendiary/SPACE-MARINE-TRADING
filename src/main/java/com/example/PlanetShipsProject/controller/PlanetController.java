c361ff2bb57f4c50e7750bbae72f70aefd180b31
vmoskaltsev <vmoskaltsev@gmail.com>
2024-11-28 17:17:59 +0200

    1

--- src/main/java/com/example/PlanetShipsProject/controller/PlanetController.java
+++ src/main/java/com/example/PlanetShipsProject/controller/PlanetController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("a/pi/planets")
+@RequestMapping("/api/planets")
 public class PlanetController {
     final PlanetService planetService;
     @Autowired
