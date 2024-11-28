9883fa7c656c1e6f59db5b47994dd877c4b1c231
vmoskaltsev <vmoskaltsev@gmail.com>
2024-11-28 11:56:58 +0200

    1

--- src/main/java/com/example/PlanetShipsProject/service/PlanetService.java
+++ src/main/java/com/example/PlanetShipsProject/service/PlanetService.java
@@ -1,31 +1,33 @@
 package com.example.PlanetShipsProject.service;
-
 import com.example.PlanetShipsProject.model.Planet;
 import com.example.PlanetShipsProject.repository.PlanetRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.List;
+
 @Service
 public class PlanetService {
     private final PlanetRepository planetRepository;
 
-    public findAllPlanets(Planet planet){
-        planetRepository.findAll();
+    public List<Planet> findAllPlanets(){
+       return planetRepository.findAll();
     }
-    public createPlanet(Planet planet){
+    public Planet createPlanet(Planet planet){
         return planetRepository.save(planet);
     }
-    public getPlanetById(Long id){
-        return planetRepository.findById(id);
+    public Planet getPlanetById(Long id){
+        return planetRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Нема планеты с индификатором "+id));
     }
-    public deletePlanet (Long id){
-        planetRepository.delete(id);
+    public void deletePlanet (Long id){
+        planetRepository.deleteById(id);
     }
-
+    @Autowired
+    public PlanetService(PlanetRepository planetRepository) {
+        this.planetRepository = planetRepository;
+    }
 }
 
