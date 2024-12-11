1aff7c347a6e0402006f02e24ca56c9cabea87fc
Vladislav Moskaltsev <jane.boichuk@gmail.com>
2024-12-11 08:34:20 +0200

    1

--- src/main/java/com/example/PlanetShipsProject/Mapper/PlanetResourceMapping.java
+++ src/main/java/com/example/PlanetShipsProject/Mapper/PlanetResourceMapping.java
@@ -1,4 +1,27 @@
 package com.example.PlanetShipsProject.Mapper;
 
+import com.example.PlanetShipsProject.dto.PlanetResourceDTO;
+import com.example.PlanetShipsProject.model.PlanetResource;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlanetResourceMapping {
+    public PlanetResourceDTO planetResourceEntityToDto(PlanetResource planetResource){
+        PlanetResourceDTO planetResourceDTO = new PlanetResourceDTO();
+        planetResourceDTO.setId(planetResource.getId());
+        planetResourceDTO.setName(planetResource.getName());
+        planetResourceDTO.setRarity(planetResource.getRarity());
+        planetResourceDTO.setListOfPlanets(planetResource.getListOfPlanets());
+        planetResourceDTO.setListOfShips(planetResource.getListOfShips());
+        return planetResourceDTO;
+    }
+    public PlanetResource planetResourceDtoToEntity(PlanetResourceDTO planetResourceDTO){
+        PlanetResource planetResource = new PlanetResource();
+        planetResource.setId(planetResourceDTO.getId());
+        planetResource.setName(planetResourceDTO.getName());
+        planetResource.setRarity(planetResourceDTO.getRarity());
+        planetResource.setListOfPlanets(planetResourceDTO.getListOfPlanets());
+        planetResource.setListOfShips(planetResourceDTO.getListOfShips());
+        return planetResource;
+    }
 }
--- src/main/java/com/example/PlanetShipsProject/dto/PlanetResourceDTO.java
+++ src/main/java/com/example/PlanetShipsProject/dto/PlanetResourceDTO.java
@@ -1,4 +1,16 @@
 package com.example.PlanetShipsProject.dto;
 
+import com.example.PlanetShipsProject.model.Planet;
+import com.example.PlanetShipsProject.model.SpaceShip;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.List;
+@Data
 public class PlanetResourceDTO {
+    private Long id;
+    private String name;
+    private Character rarity;
+    List<SpaceShip> listOfShips;
+    List<Planet> listOfPlanets;
 }
